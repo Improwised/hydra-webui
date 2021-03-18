1708d3b7ee6eb905a89f64fa281889c00ecbb669
Vishal <vishal@improwised.com>
2021-03-18 12:50:07 +0530

    Changed delete modal

--- web/components/Layout/NavBar.vue
+++ web/components/Layout/NavBar.vue
@@ -2,10 +2,8 @@
   <div>
     <!-- Just an image -->
     <b-navbar variant="faded" type="light">
-      <b-navbar-brand href="#">
-        <nuxt-link to="/">
-          <img src="@/assets/images/banner_hydra.png" height="70" />
-        </nuxt-link>
+      <b-navbar-brand href="/">
+        <img src="@/assets/images/banner_hydra.png" height="70" />
       </b-navbar-brand>
     </b-navbar>
   </div>
